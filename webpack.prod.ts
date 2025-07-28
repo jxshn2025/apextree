435c85e76186c6226de7b2c5874a2bad7900679f
bhaumikspanchal <bhaumik.panchal@zohomail.in>
2025-07-28 17:27:51 +0530

    code cleanup

--- webpack.prod.ts
+++
@@ -1,9 +0,0 @@
-import {Configuration} from 'webpack';
-import {commonConfig} from './webpack.common';
-
-const config: Configuration = {
-  ...commonConfig,
-  mode: 'production',
-};
-
-export default config;
